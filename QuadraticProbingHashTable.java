996c14ee9f2316f65fc391101adce1ccde03414c
Sam Buck <swb5075@uncw.edu>
2015-04-26 20:10:27 -0400

    Update QuadraticProbingHashTable.java

--- QuadraticProbingHashTable.java
+++ QuadraticProbingHashTable.java
@@ -1,22 +1,4 @@
 import java.util.Random;
-
-
-// QuadraticProbing Hash table class
-//
-// CONSTRUCTION: an approximate initial size or default of 101
-//
-// ******************PUBLIC OPERATIONS*********************
-// bool insert( x )       --> Insert x
-// bool remove( x )       --> Remove x
-// bool contains( x )     --> Return true if x is present
-// void makeEmpty( )      --> Remove all items
-
-
-/**
- * Probing table implementation of hash tables.
- * Note that all "matching" is based on the equals method.
- * @author Mark Allen Weiss
- */
 public class QuadraticProbingHashTable<AnyType>
 {
     /**
@@ -225,34 +207,4 @@ public class QuadraticProbingHashTable<AnyType>
 
         return true;
     }
-
-
-    // Simple main
-    public static void main( String [ ] args )
-    {
-    	
-        QuadraticProbingHashTable<String> H = new QuadraticProbingHashTable<String>( );
-
-        final int NUMS = 400000;
-        final int GAP  =   37;
-
-        System.out.println( "Checking... (no more output means success)" );
-
-
-        for( int i = GAP; i != 0; i = ( i + GAP ) % NUMS )
-            H.insert( ""+i );
-        for( int i = 1; i < NUMS; i+= 2 )
-            H.remove( ""+i );
-
-        for( int i = 2; i < NUMS; i+=2 )
-            if( !H.contains( ""+i ) )
-                System.out.println( "Find fails " + i );
-
-        for( int i = 1; i < NUMS; i+=2 )
-        {
-            if( H.contains( ""+i ) )
-                System.out.println( "OOPS!!! " +  i  );
-        }
-    }
-
 }
